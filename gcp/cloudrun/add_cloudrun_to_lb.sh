01e86d4c28bd42a1de67d7a305f649e0fc049240
Chris Chu <c20711@gmail.com>
2024-08-29 18:49:20 +0800

    Move all error message from main to function.sh

--- gcp/cloudrun/add_cloudrun_to_lb.sh
+++ gcp/cloudrun/add_cloudrun_to_lb.sh
@@ -13,8 +13,8 @@ domain="$5"
 # Print usage instructions
 print_usage() {
     echo "Your input: \"$0\" \"$cr_name\" \"$project\" \"$region\" \"$load_balancer\" \"$domain\" is invalid, please check again"
-    echo "Usage: $0 <cloud_run_service_name> <project> <region>"
-    echo "Example: $0 actualprices-api-dev-mp sinyi-cloud asia-east1"
+    echo "Usage: $0 <cloud_run_service_name> <project> <region> <load_balanacer> <domain>"
+    echo "Example: $0 actualprices-api-dev-mp sinyi-cloud asia-east1 cr-ap-sinyi-cc-ilb docuhouseagency-web-dev.ap.sinyi.cc"
 }
 
 # Main function
@@ -24,50 +24,27 @@ main() {
         print_usage
         exit 1
     fi
+    # Check if region is valid, if not, exit
+    check_regions
 
-    # Check if region is valid
-    check_regions || {
-        echo "Error: Region \"${region}\" is not valid."
-        exit 1
-    }
-
-    # Check if the Cloud Run service exists
-    check_cr || {
-        echo "Error: Cloud Run service \"${cr_name}\" does not exist."
-        exit 1
-    }
+    # Check if the Cloud Run service exists, if not, exit
+    check_cr || exit 1
 
     # Check NEG, create if it doesn't exist
-    check_neg || {
-        echo "${cr_name}-endpoint does not exist. Creating..."
-        create_neg || {
-            echo "Error: Failed to create ${cr_name}-endpoint."
-            exit 1
-        }
-    }
+    check_neg || create_neg
 
     # Check backend, create if it doesn't exist
     check_backend || {
-        echo "${cr_name}-backend does not exist. Creating..."
-        create_backend || add_backend || {
-            echo "Error: Failed to create ${cr_name}-backend."
-            exit 1
-        }
-    }
-    # Check Load Balancer, then check domain, if not exists, add URL map
-    check_lb || {
-        echo "Error: Load Balancer \"${load_balancer}\" does not exist."
-        exit 1
+        create_backend && add_backend
     }
 
+    # Check Load Balancer, if not, exit
+    check_lb || exit 1
+
+    # Check if the domain exists in the Load Balancer, if not exists, add URL map
     check_lb_domain || {
-        echo "Domain \"${domain}\" does not exist in the Load Balancer \"${load_balancer}\". Adding URL map..."
-        add_urlmap || {
-            echo "Error: Failed to add URL map for domain \"${domain}\" to Load Balancer \"${load_balancer}\"."
-            exit 1
-        }
+        add_urlmap || exit 1
     }
 }
 
-# Execute main function with arguments
 main "$@"
--- gcp/lib/cloudrun_functions.sh
+++ gcp/lib/cloudrun_functions.sh
@@ -1,8 +1,12 @@
-# Check if the cloud run exists
-
-# To be implemented
 # check_projects() {}
 
+print_error() {
+  local message="$1"
+  echo "Error: $message"
+  exit 1
+}
+
+# Check if the region is valid
 check_regions() {
     local valid_regions=(
         "africa-south1" "asia-east1" "asia-east2" "asia-northeast1" "asia-northeast2" "asia-northeast3"
@@ -11,24 +15,26 @@ check_regions() {
         "europe-west2" "europe-west3" "europe-west4" "europe-west6" "europe-west8" "europe-west9"
         "me-central1" "me-central2" "me-west1" "northamerica-northeast1" "northamerica-northeast2"
         "southamerica-east1" "southamerica-west1" "us-central1" "us-east1" "us-east4" "us-east5"
-        "us-south1" "us-west1" "us-west2" "us-west3" "us-west4"
+        "us-south1" "us-west1" "us-west2" "us-west3" "us-west4" "global"
     )
 
     for valid_region in "${valid_regions[@]}"; do
         if [[ "$region" == "$valid_region" ]]; then
+            echo "Region: \"${region}\" check passed."
             return 0 # Region is valid
         fi
     done
-
-    return 1 # Region is not valid
+    print_error "Region: \"${region}\" doesn't exist."
 }
 
+# Check if the cloud run exists
 check_cr() {
     local output=$(gcloud run services list --project="${project}" --region="${region}" --filter="metadata.name=${cr_name}" --format="value(metadata.name)")
     if [ -n "$output" ]; then
-        echo "Cloud Run check passed. \"${cr_name}\" exists"
+        echo "Cloud Run: \"${cr_name}\" check passed."
         return 0 # Exists
     else
+        echo "Cloud Run: \"${cr_name}\" doesn't exist."
         return 1 # Not Exists
     fi
 }
@@ -44,9 +50,10 @@ check_neg() {
     )
 
     if [ -n "$output" ]; then
-        echo "\"${cr_name}-endpoint\" exists"
+        echo "Network Endpoint Group: \"${cr_name}-endpoint\" exists."
         return 0 # Exists
     else
+        echo "Network Endpoint Group: \"${cr_name}-endpoint\" doesn't exists."
         return 1 # Not Exists
     fi
 }
@@ -61,9 +68,10 @@ check_backend() {
             --format="value(name)"
     )
     if [ -n "$output" ]; then
-        echo ""${cr_name}-backend" exists"
+        echo "Backend: \"${cr_name}-backend\" exists."
         return 0 # Exists
     else
+        echo "Backend: \"${cr_name}-backend\" doesn't exists."
         return 1 # Not Exists
     fi
 }
@@ -73,9 +81,10 @@ check_lb() {
     local output=$(gcloud compute url-maps list --project="${project}" --filter="name:${load_balancer}" --format="value(name)")
 
     if [ -n "$output" ]; then
-        echo "Load Balancer check passed. \"${load_balancer}\" exists"
+        echo "Load Balancer: \"${load_balancer}\" exists"
         return 0 # Exists
     else
+         echo "Load Balancer: \"${load_balancer}\" doesn't exists"
         return 1 # Not Exists
     fi
 }
@@ -90,10 +99,10 @@ check_lb_domain() {
     )
 
     if echo "$host_rules" | grep -q "'hosts': \['${domain}'"; then
-        echo "Host ${domain} exists."
+        echo "Domain: \"${domain}\" exists."
         return 0 # Exists
     else
-        echo "Host ${domain} does not exist."
+        echo "Domain: \"${domain}\" doesn't exist."
         return 1 # Not Exists
     fi
 }
@@ -107,9 +116,10 @@ create_neg() {
         --cloud-run-service="${cr_name}"
 
     if [ $? -eq 0 ]; then
-        echo "Successfully created ${cr_name}-endpoint"
+        echo "Successfully create Network Endpoint Group: \"${cr_name}-endpoint\"."
         return 0 # Create Successfully
     else
+        print_error "Fail to create Network Endpoint Group: \"${cr_name}-endpoint\"."
         return 1 # Failuare
     fi
 }
@@ -125,10 +135,10 @@ create_backend() {
         --logging-sample-rate=1
 
     if [ $? -eq 0 ]; then
-        echo "Successfully created ${cr_name}-backend"
+        echo "Successfully created \"${cr_name}-backend\""
         return 0 # Create Successfully
     else
-        echo "Failed to create ${cr_name}-backend"
+        print_error "Failed to create \"${cr_name}-backend\""
         return 1 # Failuare
     fi
 }
@@ -141,10 +151,11 @@ add_backend() {
         --network-endpoint-group=${cr_name}-endpoint \
         --network-endpoint-group-region="${region}"
 
-    if [ $? -ne 0 ]; then
-        echo "Successfully added ${cr_name}-endpoint to ${cr_name}-backend"
+    if [ $? -eq 0 ]; then
+        echo "Successfully added \"${cr_name}-endpoint\" to \"${cr_name}-backend\""
         return 0 # Create Successfully
     else
+        print_error "Failed to add \"${cr_name}-endpoint\" to \"${cr_name}-backend\"" 
         return 1 # Failuare
     fi
 }
@@ -159,9 +170,10 @@ add_urlmap() {
         --new-hosts="${domain}"
 
     if [ $? -eq 0 ]; then
-        echo "Successfully created Add \"${domain}\" to \"${load_balancer}\""
+        echo "Successfully added \"${domain}\" to \"${load_balancer}\" with backend: \"${cr_name}-backend\""
         return 0 # Create Successfully
     else
+        print_error "Failed to add \"${domain}\" to \"${load_balancer}\" with backend: \"${cr_name}-backend\""
         return 1 # Failuare
     fi
 }
