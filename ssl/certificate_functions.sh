63195be3f4a2c6868ba23b20bb0c7fdf38fef82e
Chris Chu <c20711@gmail.com>
2024-09-05 14:24:44 +0800

    rename and update ssl script

---
+++ ssl/certificate_functions.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# Check if the certificate and key exist
+check_certificate_and_key() {
+    local cert="$1"
+    local key="$2"
+
+    if [ -f "$source_path/$cert" ] && [ -f "$source_path/$key" ]; then
+        echo "Certificate and key found."
+        return 0
+    else
+        if [ ! -f "$source_path/$cert" ]; then
+            echo "Certificate not found!"
+        fi
+        if [ ! -f "$source_path/$key" ]; then
+            echo "Key not found!"
+        fi
+        return 1
+    fi
+}
+
+is_fqdn() {
+    local common_name="$1"
+    local fqdn_regex="^(\*\.)?([a-zA-Z0-9]+(-[a-zA-Z0-9]+)*\.)+[a-zA-Z]{2,}$"
+    if [[ "$common_name" =~ $fqdn_regex ]]; then
+        echo "Valid FQDN".
+        return 0
+    else
+        echo "Invalid FQDN"
+        return 1
+    fi
+}
+
+check_certificate_and_intermediate() {
+    local cert="$1"
+    openssl verify -untrusted "$source_path/$cert" "$source_path/twca_intermediate.crt" > /dev/null 2>&1
+
+    if [ $? -eq 0 ]; then
+        echo "Certificate and intermediate are valid."
+        return 0
+    else
+        echo "Error: Certificate verification failed!"
+        return 1
+    fi
+}
+
+check_certificate_and_key_match() {
+    local cert="$1"
+    local key="$2"
+
+    # Extract the Modulus from the certificate
+    local cert_modulus=$(openssl x509 -noout -modulus -in "$source_path/$cert" | openssl md5)
+
+    # Extract the Modulus from the key
+    local key_modulus=$(openssl rsa -noout -modulus -in "$source_path/$key" | openssl md5)
+
+    # Compare the Modulus values
+    if [ "$cert_modulus" == "$key_modulus" ]; then
+        echo "Certificate and key match."
+        return 0
+    else
+        echo "Error: Certificate and key do not match!"
+        return 1
+    fi
+}
+
+# Get the certificate's end date
+get_enddate() {
+    end_date=$(openssl x509 -in "$source_path/$certificate" -enddate -noout | cut -d= -f2 | xargs -I{} date -d {} +%Y%m%d)
+    echo "$end_date"
+}
+
+# Get the certificate's common name
+get_common_name() {
+    common_name=$(openssl x509 -in "$source_path/$certificate" -noout -subject | sed -n '/^subject/s/^.*CN = //p' | cut -d'/' -f1)
+    echo "$common_name"
+}
+
+get_converted_common_name() {
+    common_name="$1"
+    # Check if the common name starts with *.
+    if [[ "$common_name" == \*.* ]]; then
+        # Replace * with wildcard and replace . with -
+        converted_common_name="wildcard-$(echo "$common_name" | sed 's/\*\.//' | tr '.' '-')"
+    else
+        converted_common_name=$(echo "$common_name" | sed 's/\*\.//' | tr '.' '-')
+    fi
+    echo "$converted_common_name"
+}
+
+rename_cert_key() {
+    local common_name="$1"
+    local end_date="$2"
+
+    cp "$source_path/$certificate" "$target_path/${common_name}-${end_date}.crt"
+    cp "$source_path/$key" "$target_path/${common_name}-${end_date}.key"
+    echo "Certificate and key renamed to \"${common_name}-${end_date}.crt\" and \"${common_name}-${end_date}.key\""
+}
+
+get_bundle_cert() {
+    local common_name="$1"
+    local end_date="$2"
+
+    cat "$source_path/twca_intermediate.crt" "$target_path/${common_name}-${end_date}.crt" > "$target_path/${common_name}-${end_date}-bundle.crt"
+    echo "Create \"${common_name}-${end_date}-bundle.crt\""
+}
--- ssl/prepare_crt.sh
+++ ssl/prepare_crt.sh
@@ -4,16 +4,17 @@
 SCRIPT_DIR=$(dirname "$0")
 
 # Source the functions script from the lib directory relative to the script's directory
-source "$SCRIPT_DIR/ssl_functions.sh"
+source "$SCRIPT_DIR/certificate_functions.sh"
 
 # Parameters
-ssl_path="$1"
-certificate="$2"
-key="$3"
+source_path="$1"
+target_path="$2"
+certificate="$3"
+key="$4"
 
 # Print usage instructions
 print_usage() {
-    echo "Your input: \"$0\" \"$ssl_path\" \"$certificate\" \"$key\" is invalid, please check again"
+    echo "Your input: \"$0\" \"$source_path\" \"$target_path\" \"$certificate\" \"$key\" is invalid, please check again"
     echo "Usage: $0 <ssl_path> <certificate> <key>"
     echo "ssl_path    : Path to the SSL directory"
     echo "certificate : Certificate file name"
@@ -23,26 +24,34 @@ print_usage() {
 
 main() {
     # Check if the correct number of arguments is passed
-    if [[ -z $ssl_path || -z $certificate || -z $key ]]; then
+    if [[ -z $source_path || -z $target_path || -z $certificate || -z $key ]]; then
         print_usage
         exit 1
     fi
 
-    # Perform operations
-    check_certificate || exit 1
+    # Input Validation: Check if the ceritificate and key exsits.
+    check_certificate_and_key "$certificate" "$key" || exit 1
+
+    # Verify the certificate and intermediate certificate
+    check_certificate_and_intermediate "$certificate" || exit 1
+
+    # Verify the certificate and key checksum
+    check_certificate_and_key_match "$certificate" "$key" || exit 1
 
     # Get the certificate common name and end date
     common_name=$(get_common_name)
     end_date=$(get_enddate)
-    echo $end_date
+
     # Confirm common name is FQDN
     is_fqdn "$common_name" || exit 1
 
     # Change the common name format to meet certificate naming
-    converted_common_name=$(get_converted_common_name )
+    converted_common_name=$(get_converted_common_name $common_name)
 
     # Rename certificate and key
     rename_cert_key "$converted_common_name" "$end_date"
+
+    get_bundle_cert "$converted_common_name" "$end_date"
 }
 
 main "$@"
